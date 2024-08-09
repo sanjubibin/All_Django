01d92ca493db3c3bc4a7b87dd6a9ba62ed881cce
sanju <sanju.a@ionixxtech.com>
2024-08-09 16:06:04 +0530

    just saving the latest code

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-data = {}
+class Employee:
+    def __init__(self, name='NA', age='NA', no='NA', intro='NA') -> None:
+        self.name = name
+        self.first_name = name.split(' ')[0] if name != 'NA' else 'NA-NND'
+        mid_name_list = [x.capitalize() for x in name.split(' ')[1:-1]]
+        self.middle_name = ' '.join(mid_name_list) if name != 'NA' and len(mid_name_list) != 0 else 'NA-NMN'
+        self.last_name = name.split(' ')[-1] if name != 'NA' else 'NA-NND'
+        self.age = age
+        self.no = no
+        self.intro = intro
 
+    def set_intro(self) -> None:
+        self.name: str
+        self.first_name: str
 
-def 
+        if not self.name.startswith('NA'):
+            self.intro = f'The Great {self.first_name.capitalize()}'
+        else:
+            print('inorder to set_intro name has to be updated already until that name will be "NA"')
